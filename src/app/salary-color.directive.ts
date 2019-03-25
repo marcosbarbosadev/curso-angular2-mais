1429686c9bb2c71020d332c36acc0c43418b5c9a
Marcos Barbosa <barbosa.mg@gmail.com>
2019-03-25 00:14:22 -0300

    usando diretiva

---
+++ src/app/salary-color.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[salaryColor]'
+})
+export class SalaryColorDirective {
+
+  @Input()
+  salaryColor;
+
+  constructor(private element: ElementRef) {
+
+    setTimeout(() => {
+      const nativeElement: HTMLElement = this.element.nativeElement;
+      const salary = parseFloat(this.salaryColor);
+      nativeElement.style.color = salary > 20000 ? 'green' : 'red';
+    }, 2000);
+
+  }
+
+}
