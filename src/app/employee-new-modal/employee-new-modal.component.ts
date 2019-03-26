722607380bcc7fd6bdf5b1d28c82296845e1e56b
Marcos Barbosa <barbosa.mg@gmail.com>
2019-03-25 22:21:55 -0300

    Usando o formulário do modal para adicionar empregado

--- src/app/employee-new-modal/employee-new-modal.component.ts
+++ src/app/employee-new-modal/employee-new-modal.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
-import {Employee} from '../employee.service';
+import {Employee, EmployeeService} from '../employee.service';
 
 declare const $;
 
@@ -16,7 +16,7 @@ export class EmployeeNewModalComponent implements OnInit {
     bonus: 0
   };
 
-  constructor(private element: ElementRef) { }
+  constructor(private element: ElementRef, private employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
@@ -26,9 +26,22 @@ export class EmployeeNewModalComponent implements OnInit {
       $(divModal).modal('show');
   }
 
+  hide() {
+    const divModal = this.getDivModal();
+    $(divModal).modal('hide');
+  }
+
   private getDivModal(): HTMLElement {
     const elementNative: HTMLElement = this.element.nativeElement;
-    return elementNative.firstChild as HTMLElement;
+    return elementNative.firstChild.firstChild as HTMLElement;
   }
+
+  addEmployee() {
+    const copy = Object.assign({}, this.employee);
+    this.employeeService.addEmployee(copy);
+    this.employee = {name: '', salary: 0, bonus: 0};
+    this.hide();
+  }
+
 
 }
